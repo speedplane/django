8d81c9f0a994a53596a470e34c525f97cf4f5736
speedplane <michael.sander@docketalarm.com>
2019-09-04 07:11:02 -0400

    When making an id for a deferred model, be sure to use the real model id, not the id of the deferred object.

--- django/dispatch/dispatcher.py
+++ django/dispatch/dispatcher.py
@@ -11,6 +11,8 @@ else:
 
 
 def _make_id(target):
+    if hasattr(target, '_deferred') and target._deferred:
+        return id(target._meta.proxy_for_model)
     if hasattr(target, '__func__'):
         return (id(target.__self__), id(target.__func__))
     return id(target)
