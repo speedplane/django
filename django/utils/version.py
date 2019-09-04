707ddc97bad65a6a232f8f28b647068bc540fd0f
speedplane <michael.sander@docketalarm.com>
2019-09-04 07:12:12 -0400

    Don't globally import subprocess. It can cause problems in some GAE environments.

--- django/utils/version.py
+++ django/utils/version.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 import datetime
 import os
-import subprocess
 
 
 def get_version(version=None):
@@ -41,6 +40,7 @@ def get_git_changeset():
     This value isn't guaranteed to be unique, but collisions are very unlikely,
     so it's sufficient for generating the development version numbers.
     """
+    import subprocess
     repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     git_log = subprocess.Popen('git log --pretty=format:%ct --quiet -1 HEAD',
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
